1c4a20e3fcbe2c8591613e74727522e38a7c73c5
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-30 11:50:31 -0600

    Moves routes to Users

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res) {
   res.send('respond with a resource');
 });
 
