823df4823731d2e2235bae0e69d3e94dc001e5f5
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-30 14:22:16 -0600

    Imports bcrypt and uuid to create a secure api key and a password

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,10 +2,31 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
+const random_key_generator = require('uuid/v4');
+
+var User = require('../../../models').User;
+
 
 /* GET users listing. */
 router.post('/', function(req, res) {
-  res.send('respond with a resource');
-});
+  var email = req.body.username
 
+  if(req.body.password === req.body.password_confirmation){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      User.create({
+        email: req.body.email,
+        password: hash,
+        api_key: random_key_generator()
+      })
+      .then( user => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).send(JSON.stringify(user.api_key));
+      })
+      .catch( error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({error});
+      })
+    })
+  }
+  
 module.exports = router;
