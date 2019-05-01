4be6a6de0209fecc181a61ac189b7484bc7782eb
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-01 11:41:13 -0600

    Makes function to return location data

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -3,9 +3,22 @@ var router = express.Router();
 var fetch = require('node-fetch');
 var pry = require('pryjs');
 
-var geocoding_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+var geocodingUrl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 var User = require('../../../models').User;
+
+var locationData = function(res, address) {
+  let locationUrl = 'address=' + address;
+  let apiKeyUrl = `&key=${process.env.GEOCODING_API_KEY}`;
+  let url = geocodingUrl + locationUrl + apiKeyUrl;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(result => res.send(result))
+    .catch(error => response.send({error}));
+};
+
+
 
 router.get('/', function(req, res) {
   User.findOne({
@@ -17,26 +30,14 @@ router.get('/', function(req, res) {
     if(!user){
       res.sendStatus(401);
     } else {
-      console.log('authorized user with api_key ' + user.api_key);
-      let location_url = 'address=' + req.query.location;
-      let api_key_url = `&key=${process.env.GEOCODING_API_KEY}`;
-      let url = geocoding_url + location_url + api_key_url;
-
-      console.log(url);
-      fetch(url)
-      .then( res => res.json())
-      .then( address_data => {
-        res.send( { address_data });
-      })
-      .catch(error => {
-        res.send({error});
-        console.log("error in fetch");
-      });
+      locationData(res, req.query.location);
     }
   })
   .catch(error => {
     res.send({error});
   });
 });
+
+
 
 module.exports = router;
