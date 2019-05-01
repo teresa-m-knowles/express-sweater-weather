2e551157d85d9caa854375e1be23f68d73d8fff7
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-01 00:21:45 -0600

    For now, the forecast endpoint just returns the JSON from the geocoding endpoing that sends the address. No forecast yet

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,10 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
+
+var geocoding_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 var User = require('../../../models').User;
 
 router.get('/', function(req, res) {
-  console.log(req.query)
+  User.findOne({
+    where: {
+      api_key: req.body.api_key
+    }
+  })
+  .then( user => {
+    debugger;
+    if(!user){
+      res.sendStatus(401);
+    } else {
+      console.log('authorized user with api_key ' + user.api_key);
+      let location_url = 'address=' + req.query.location;
+      let api_key_url = `&key=${process.env.GEOCODING_API_KEY}`;
+      let url = geocoding_url + location_url + api_key_url;
+      console.log(url);
+      fetch(url)
+      .then( res => res.json())
+      .then( coordinates => {
+        res.send( { coordinates });
+      })
+      .catch(error => {
+        res.send({error});
+        console.log("error in fetch");
+      });
+    }
+  })
+  .catch(error => {
+    res.send({error});
+  });
 });
 
 module.exports = router;
