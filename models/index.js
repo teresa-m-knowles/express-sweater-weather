a8f3fe2954584eb4cab6805098f26eedfcd6bfc3
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-01 12:55:23 -0600

    Adds dialectOptions to config

--- models/index.js
+++ models/index.js
@@ -16,13 +16,6 @@ if (config.use_env_variable) {
 }
 
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-
 fs
   .readdirSync(__dirname)
   .filter(file => {
