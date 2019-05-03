b89878e1fde64c0d5739b7aa6fcb2e4785f01f75
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-02 20:30:47 -0600

    Associates Users and Locations with a belongsToMany in their associations

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function(models) {
     // associations can be defined here
+    User.belongsToMany(Location, {through: 'Favorite'});
   };
   return User;
 };
