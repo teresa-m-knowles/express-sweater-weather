676a74df24be3842d0be0959532b93245955d3d2
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-02 18:58:30 -0600

    User can get forecast

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -7,40 +7,42 @@ var geocodingUrl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 var User = require('../../../models').User;
 
-var locationData = function(res, address) {
-  let locationUrl = 'address=' + address;
-  let apiKeyUrl = `&key=${process.env.GEOCODING_API_KEY}`;
-  let url = geocodingUrl + locationUrl + apiKeyUrl;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(result => res.send(result))
-    .catch(error => response.send({error}));
-};
-
-
-var forecast = function(res, lat, lng){
-
-};
-
 router.get('/', function(req, res) {
+  let lat;
+  let lng;
+  let locationUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GEOCODING_API_KEY}`;
   User.findOne({
     where: {
       api_key: req.body.api_key
     }
   })
-  .then( user => {
-    if(!user){
-      res.sendStatus(401);
-    } else {
-      locationData(res, req.query.location);
-    }
-  })
-  .catch(error => {
-    res.send({error});
+      .then( user => {
+        if(!user){
+          res.sendStatus(401);
+        } else{
+            fetch(locationUrl)
+              .then(response => {
+                return response.json()
+              })
+              .then(data => {
+                console.log(data);
+                lat = data.results[0].geometry.location.lat
+                lng = data.results[0].geometry.location.lng
+                let forecastUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${lat},${lng}?exclude=[minutely,alerts,flags]`;
+                fetch(forecastUrl)
+                  .then(response => {
+                    return response.json();
+                  })
+                  .then(forecastData => {
+                    res.status(200).send(forecastData);
+                  })
+                  .catch(error => {
+                    res.send(error);
+                  });
+              });
+          }
+      })
+      .catch(error => console.log(error));
   });
-});
-
-
 
 module.exports = router;
